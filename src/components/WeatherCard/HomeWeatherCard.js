54dd00143b3c04925aa0c3ad7d302eeee353a742
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-16 18:45:34 -0600

    fix: repair more/less info section

--- src/components/WeatherCard/HomeWeatherCard.js
+++ src/components/WeatherCard/HomeWeatherCard.js
@@ -3,8 +3,7 @@ import { useState } from 'react'
 
 
 function HomeWeatherCard(props) {
-const [moreInfo, setMoreInfo] = useState('true')
-const [lessInfo, setLessInfo] = useState('false')
+const [moreInfo, setMoreInfo] = useState(true)
 let moreInformation = <section>
       <h3>Feels Like: {props.currentFeelsLike} F</h3>
       <h3>Cloud Cover: {props.currentCloudCover}%</h3>
@@ -13,24 +12,22 @@ let moreInformation = <section>
       </section>
 
   function clickedButtonMoreInfo() {
-    setMoreInfo('false')
-    setLessInfo('true')
+    setMoreInfo(false)
   }
 
   function clickedButtonLessInfo() {
-    setMoreInfo('true')
-    setLessInfo('false')
+    setMoreInfo(true)
   }
 
   return (
     <div>
       <section className='current-weather-card'>
         <h3>{props.currentTemp}</h3>
-        {!lessInfo && moreInformation}
       </section>
       <section className='more-information'>
-       {!moreInfo && <button onClick={clickedButtonMoreInfo}>More Info</button>}
-       {moreInfo && <button onClick={clickedButtonLessInfo}>Less Info</button>}
+       {!moreInfo && moreInformation}
+       {moreInfo && <button onClick={clickedButtonMoreInfo}>More Info</button>}
+       {!moreInfo && <button onClick={clickedButtonLessInfo}>Less Info</button>}
       </section>
     </div>
   )
