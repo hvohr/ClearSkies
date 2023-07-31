77df0e2e4af9d5d218e593b718ee0eb4feea9cc9
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-31 09:00:19 -0600

    feat: add more information skeleton for more info functionality

--- src/components/WeatherCard/DailyWeatherCard.js
+++ src/components/WeatherCard/DailyWeatherCard.js
@@ -2,6 +2,22 @@ import '../../pages/pages.css'
 import { useState } from 'react'
 
 function DailyWeatherCard(props) {
+  const [moreDailyInfo, setMoreDailyInfo] = useState(true)
+  let moreDailyInformation = <section>
+    {(props.changedState === "...") === false && <h3>Feels Like: &nbsp;&nbsp;  {props.currentFeelsLike} F</h3>}
+    {(props.changedState === "...") === false && <h3>Cloud Cover:  &nbsp;&nbsp; {props.currentCloudCover}%</h3>}
+    {(props.changedState === "...") === false && <h3>UV Index:  &nbsp;&nbsp; {props.currentUVI}</h3>}
+    {(props.changedState === "...") === false && <h3>Wind Speed:  &nbsp;&nbsp; {props.currentWindSpeed} mph</h3>}
+  </section>
+
+  function clickedButtonMoreInfo() {
+    setMoreDailyInfo(false)
+  }
+
+  function clickedButtonLessInfo() {
+    setMoreDailyInfo(true)
+  }
+
   let filterDays = props.daily.map((day) => {
     var dayname = new Date(day.dt * 1000).toLocaleDateString()
     return (
@@ -11,7 +27,9 @@ function DailyWeatherCard(props) {
         <img className='weather-icons' src={(`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`)}></img>
         <h3>{day.temp.day} F</h3>
         <h3 className='daily-summary'>{day.weather[0].description}</h3>
-        <button className='more-info-buttons'>More Info</button>
+        {!moreDailyInfo && moreDailyInformation}
+        {moreDailyInfo && <button className='more-info-buttons' onClick={clickedButtonMoreInfo}>More Info</button>}
+        {!moreDailyInfo && <button className='more-info-buttons' onClick={clickedButtonLessInfo}>Less Info</button>}
       </section>
     )
   })
