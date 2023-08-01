7378a8dab0ac391695dc12f9f7f6119eb7564713
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-31 20:22:46 -0600

    fix: remove flash of previous searched cities in daily

--- src/components/CityOptions/CityOptions.js
+++ src/components/CityOptions/CityOptions.js
@@ -4,6 +4,7 @@ function CityOptions(props) {
       {(props.cityList && props.changed === true) && props.cityList.map((city) => <button onClick={() => {
         props.showedButtons()
         props.getNewCoordinates(city.lon, city.lat, city.state)
+        props.setButtonList([])
       }} className='city-button'>{city.name}, {city.state}</button>)}
       {(!props.cityList) && <h1>Please enter a valid US City</h1>}
     </section>
