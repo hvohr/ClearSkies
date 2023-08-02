94a6203bd2aa80bf056f1e6d2774abe68f09b870
Hollis Vohr <hollis.vohr@gmail.com>
2023-08-02 11:42:46 -0600

    feat: add key prop for alert notifications

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -144,7 +144,7 @@ function Home(props) {
         <section className='current-weather-container'>
           {!changedCity && <h1>Loading...</h1>}
           <div className='alert-container'>
-            {(alert.length !== 0 && !showButtons) && alert.alerts.map((a) => <h3 style={{ border: "2px solid red" }} className='weather-alert'>{a.event}</h3>)}
+            {(alert.length !== 0 && !showButtons) && alert.alerts.map((a) => <h3 key={Date.now() + alert.alerts.indexOf(a)} style={{ border: "2px solid red" }} className='weather-alert'>{a.event}</h3>)}
           </div>
           {(changedCity && changedState !== '...') && <h1 style={{ textDecoration: 'underline' }} className='front-card-title'>Current Weather for {changedCity}, {changedState}</h1>}
           <HomeWeatherCard changedState={changedState} currentWeatherIcon={currentWeatherIcon} currentTemp={currentTemp} currentDescription={currentDescription} currentWindSpeed={currentWindSpeed}
