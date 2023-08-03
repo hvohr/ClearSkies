371cfc26707d24c51e98ea31ed686c3015cde470
Hollis Vohr <hollis.vohr@gmail.com>
2023-08-03 13:02:57 -0600

    feat: add home cypress test for nav

--- cypress/e2e/home-page.cy.js
+++ cypress/e2e/home-page.cy.js
@@ -1,22 +1,25 @@
 beforeEach(() => {
   cy.intercept("GET", 'https://api.openweathermap.org/data/3.0/onecall*', {
-    statusCode: 200,
-    fixture: "weatherData.json"
-  }).as('allweather').visit("http://localhost:3000/")
-})
-// beforeEach(() => {
-//     cy.intercept("GET", 'https://api.openweathermap.org/geo/1.0/direct*', {
-//       statusCode: 200,
-//       fixture: "weatherData.json"
-//     }).as('citynameweather').visit("http://localhost:3000/")
-//   })
+      statusCode: 200,
+      fixture: "weatherData.json"
+    }).as('allweather')
 
+    cy.intercept("GET", 'https://api.openweathermap.org/geo/1.0/direct*', {
+      statusCode: 200,
+      fixture: "weatherData.json"
+    }).as('citynameweather')
+
+    cy.intercept("GET", 'https://api.openweathermap.org/geo/1.0/reverse*', {
+      statusCode: 200,
+      fixture: "weatherData.json"
+    }).as('longlatweather')
+})
 
 describe('User should see a home page with proper navigation elements', () => {
   it('Should have a title', () => {
-    cy.wait('@allweather')
-      .wait('@citynameweather')
-      .get(".nav-title")
-      .contains("ClearSkies")
+    cy.visit("http://localhost:3000/?delay=500")
+    cy.wait('@longlatweather')
+    cy.get(".nav-title")
+    cy.contains("ClearSkies")
   })
 })
