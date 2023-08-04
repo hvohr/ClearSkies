6c5795d51c9836dd276a3f614128087b03835b18
Hollis Vohr <hollis.vohr@gmail.com>
2023-08-04 10:02:55 -0600

    test: add form error handling tests

--- cypress/e2e/error-handling.cy.js
+++ cypress/e2e/error-handling.cy.js
@@ -15,3 +15,34 @@ beforeEach(() => {
   }).as('locationData')
 })
 
+describe('Should have proper error handling in forms and fetch functions', () => {
+  it('Should not let a user enter wrong information into home form', () => {
+    cy.visit("http://localhost:3000/")
+      .wait('@locationData')
+      .wait('@cityData')
+      .wait('@latLongData')
+      .get('.form-button').click()
+      .get('.empty-error').should('be.visible')
+    cy.intercept("GET", 'https://api.openweathermap.org/geo/1.0/direct*', {
+      statusCode: 200,
+      fixture: "newCityData.json"
+    }).as('newCityData')
+      .get('.form-input').type('noexistingcity').get('.form-button').click()
+      .get('.empty-error').should('be.visible')
+  })
+  it('Should not let a user enter wrong information into daily form', () => {
+    cy.visit("http://localhost:3000/dailyforecast")
+      .wait('@locationData')
+      .wait('@cityData')
+      .wait('@latLongData')
+      .get('.form-button').click()
+      .get('.empty-error').should('be.visible')
+    cy.intercept("GET", 'https://api.openweathermap.org/geo/1.0/direct*', {
+      statusCode: 200,
+      fixture: "newCityData.json"
+    }).as('newCityData')
+      .get('.form-input').type('noexistingcity').get('.form-button').click()
+      .get('.empty-error').should('be.visible')
+  })
+})
+
