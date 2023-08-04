bee3029a7113cf4e2edb0c8be819fcfcd3506dd9
Hollis Vohr <hollis.vohr@gmail.com>
2023-08-04 10:15:21 -0600

    test: add fetch error handling testing

--- cypress/e2e/error-handling.cy.js
+++ cypress/e2e/error-handling.cy.js
@@ -44,5 +44,28 @@ describe('Should have proper error handling in forms and fetch functions', () =>
       .get('.form-input').type('noexistingcity').get('.form-button').click()
       .get('.empty-error').should('be.visible')
   })
+  it('Should show error image and message when user goes to the wrong route/url', () => {
+    cy.visit("http://localhost:3000/nonexistingpage")
+      .get('.error-icon').should('be.visible')
+      .get('nav').should('be.visible')
+      .get('.home-button').click()
+      .url("http://localhost:3000/")
+  })
+  it('Should show an error image and message on home page when a fetch issue occurs', () => {
+    cy.visit("http://localhost:3000/")
+    cy.intercept("GET", 'https://api.openweathermap.org/data/3.0/onecall*', {
+      statusCode: 404,
+      fixture: "latLongData.json"
+      }).as('fetchFail')
+      .get('.fetch-failed-image, .fetch-failed-response').should('be.visible')
+  })
+  it('Should show an error image and message on daily page when a fetch issue occurs', () => {
+    cy.visit("http://localhost:3000/dailyforecast")
+    cy.intercept("GET", 'https://api.openweathermap.org/data/3.0/onecall*', {
+      statusCode: 404,
+      fixture: "latLongData.json"
+      }).as('fetchFail')
+      .get('.fetch-failed-image, .fetch-failed-response').should('be.visible')
+  })
 })
 
